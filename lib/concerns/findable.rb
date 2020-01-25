6a47ee22f4ebf83a67801e37e6881aa69f37d385
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-01-25 00:40:39 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,11 +1,7 @@
 module Findable
-  
-  def self.find_by_name(name)
-    @@artists.detect {|a| a.name == name}
-  end  
-
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
+  module ClassMethods
+    def find_by_name(name)
+      self.all.detect{|o| o.name}
+    end    
   end
-
 end
